d11e477d8482dd7846d0f6a657e2c685c79f6222
franzgeraldroque <franzgeraldroque17@gmail.com>
2026-05-30 15:40:38 +0800

    This is my file for my Finals Project

--- Roque_FinalsExcel.py
+++ Roque_FinalsExcel.py
@@ -4,21 +4,23 @@ workbook = op.Workbook()
 sheet = workbook.active
 
 sheet["A1"] = "Customer ID"
-sheet["B1"] = "First Name"
-sheet["C1"] = "Middle Name"
-sheet["D1"] = "Last Name"
+sheet["B1"] = "Last Name"
+sheet["C1"] = "First Name"
+sheet["D1"] = "Middle Name"
 sheet["E1"] = "Age"
 sheet["F1"] = "Height (In cm)"
 sheet["G1"] = "Weight (In kg)"
-sheet["G1"] = "BMI"
-sheet["G1"] = "Streak Days"
+sheet["H1"] = "BMI"
+sheet["I1"] = "Streak Days"
 
-# sheet["A2"] = 1
-# sheet["B2"] = "Franz Gerald Roque"
-# sheet["C2"] = 18
-# sheet["D2"] = 167
-# sheet["E2"] = 65
-# sheet["F2"] = 23.3
-# sheet["G2"] = 12
+sheet["A2"] = 1
+sheet["B2"] = "Roque"
+sheet["C2"] = "Franz Gerald"
+sheet["D2"] = "L."
+sheet["E2"] = 18
+sheet["F2"] = 167
+sheet["G2"] = 65
+sheet["H2"] = 23.3
+sheet["I2"] = 1
 
 workbook.save("Roque_Database.xlsx")
